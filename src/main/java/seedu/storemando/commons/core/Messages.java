b0fed561ec1204b878adc504b7ce212e9843bc62
Md-Fazil <fuzyll10598@gmail.com>
2021-04-10 10:23:51 +0800

    Add next line character to warnings

--- src/main/java/seedu/storemando/commons/core/Messages.java
+++ src/main/java/seedu/storemando/commons/core/Messages.java
@@ -11,6 +11,6 @@ public class Messages {
     public static final String MESSAGE_MORE_THAN_ONE_ITEM_LISTED_OVERVIEW = "%1$d items listed!";
     public static final String MESSAGE_LESS_THAN_TWO_ITEMS_LISTED_OVERVIEW = "%1$d item listed!";
     public static final String MESSAGE_DUPLICATE_ITEM = "This item already exists in the storemando";
-    public static final String MESSAGE_ITEM_EXPIRED_WARNING = "Warning: Item has already expired!";
-    public static final String MESSAGE_SIMILAR_ITEM_WARNING = "Warning: Similar item exists in the same location!";
+    public static final String MESSAGE_ITEM_EXPIRED_WARNING = "\nWarning: Item has already expired!";
+    public static final String MESSAGE_SIMILAR_ITEM_WARNING = "\nWarning: Similar item exists in the same location!";
 }
